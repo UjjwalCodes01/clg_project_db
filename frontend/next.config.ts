bb6239dd7a173fb2b62401fa32bcbbaae0ba0cb5
Ujjwal Tyagi <ujjwaltyagi9605@gmail.com>
2025-12-03 20:18:02 +0530

    changes for ui

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,24 +1,8 @@
+import type { NextConfig } from "next";
 
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-  // The actual fix
-  assetPrefix: '',
-  basePath: '',
-  trailingSlash: true,
-
-  // Prevent WebView `_next` blocking
-  compiler: {
-    removeConsole: false,
-  },
-
-  // Important for Android WebView
-  experimental: {
-    forceSwcTransforms: true,
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
+
